1011be190f36990f315c46369d3349bd8cec4ef9
vigarp <vigar.8421@outlook.com>
2022-08-18 19:24:08 +0700

    minor fix directories and add readme.md

---
+++ pertemuan 1/README.md
@@ -0,0 +1,6 @@
+How to run:
+1. download this file test.sh
+2. run bash test.sh (url here)
+3. cat the file in tugas/tugas1.txt
+
+if you run multiple times, it will generately by add the content in tugas1.txt
--- pertemuan 1/test.sh
+++ pertemuan 1/test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 output=$(curl -i "$1")
+DIR="/home/$USER/CS/pertemuan 1/tugas"
 
-if [ -d "/home/vigar/CS/tugas" ]; #please change the user directory
+if [ "$1" ];
 then
-	echo "$output" >> tugas/tugas1.txt
-else 
-	mkdir tugas && echo "$output" >> tugas/tugas1.txt
+	
+	if [ -d "$DIR" ];
+	then
+        echo "$output" >> tugas/tugas1.txt
+	else 
+        mkdir tugas && echo "$output" >> tugas/tugas1.txt
+	fi
+
+else
+	echo "Error: parameter is empty"
+	exit 1
 fi
 
 echo "done, see cat tugas/tugas1.txt"
